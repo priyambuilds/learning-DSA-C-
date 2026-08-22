54fc79fd2bdf7ec9ee9f20fe14672695772d9550
priyambuilds <dpriyam45@gmail.com>
2026-08-22 14:20:28 +0530

    update

--- DSA/3-loops.cpp
+++ DSA/3-loops.cpp
@@ -1,10 +1,97 @@
+
+// int main() {
+    //     int n;
+    //     cin >> n;
+    //     for (int i = n; i > 0; i--) {
+        //         cout << i << " ";
+        //     }
+        // }
+        
+// #include<iostream>
+// using namespace std;
+// int main () {
+//     int n;
+//     cin >> n;
+//     for (int i = 2; i <= n; i++) {
+//         if(i%2==0) {
+//             cout << i << " ";
+//         }
+//     }
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main () {
+//     for (char i = 'A'; i <= 'Z'; i++)
+//     {
+//         cout<<i<<" ";
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+// #include <vector>
+
+// int main () {
+//     int a;
+//     cin >> a;
+//     vector<int> nums(static_cast<size_t>(a));
+//     int pos = 0, neg = 0, eve = 0, odd = 0;
+//     for (int i = 0; i < a; i++)
+//     {
+//         cin >> nums[static_cast<size_t>(i)];
+//     }
+//     for(int i : nums) {
+//         if (i > 0)  pos += 1;
+//         if (i < 0) neg += 1;
+//         if (i % 2 == 0) eve += 1;
+//         else odd += 1;
+//     }
+//     cout << pos << endl
+//          << neg << endl
+//          << eve << endl
+//          << odd;
+// }
+
+// int main () {
+    //     int a;
+    //     cin >> a;
+    //     int pos = 0, neg = 0, eve = 0, odd = 0;
+    //     for (int i = 1; i <= a; i++) {
+        //         int b;
+        //         cin >> b;
+        //         if (b > 0)  pos++;
+        //         if (b < 0) neg++;
+        //         if (b % 2 == 0) eve++;
+        //         else odd++;
+        
+        //     }
+        //         cout << pos << endl
+        //          << neg << endl
+        //          << eve << endl
+        //          << odd;
+        // }
+        
+// #include<iostream>
+// using namespace std;
+// int main () {
+//     int a;
+//     cin >> a;
+//     int sum = 0;
+//     for (int i = 1; i <= a; i++)
+//     {
+//         sum += i;
+//     }
+//     cout << sum;
+// }
+
 #include<iostream>
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    for (int i = n; i > 0; i--) {
-        cout << i << " ";
-    }
+int main () {
+    long long a;
+    cin >> a;
+    long long sum = a*(a+1)/2;
+    cout << sum;
 }
--- DSA/main.js
+++ DSA/main.js
@@ -11,3 +11,33 @@ const Calculate = (a, b, c) => {
     )
 }
 Calculate(a, b, c)
+
+let n = Number(prompt("enter a no"))
+let arr = Array(n)
+const Sort = (arr) => {
+    for (let i = 0; i < n; i++) {
+        let a = Number(prompt(("enter nos in arr")))
+        arr[i] = a
+    }
+    let pos = 0
+    let neg = 0
+    let eve = 0
+    let odd = 0
+    for (i of arr) {
+        if (i > 0) {
+            pos += 1
+        }
+        if (i < 0) {
+            neg += 1
+        }
+        if (i % 2 == 0) {
+            eve += 1
+        }
+        else {
+            odd += 1
+        }
+    }
+    console.log(arr)
+    return console.log(`${pos}, ${neg}, ${eve}, ${odd}`)
+}
+Sort(arr)
