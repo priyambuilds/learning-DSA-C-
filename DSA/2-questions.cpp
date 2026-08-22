4527442ccb65dcce75e62b64c147a807d6494e92
priyambuilds <dpriyam45@gmail.com>
2026-08-22 10:24:47 +0530

    solved some problems

--- DSA/2-questions.cpp
+++ DSA/2-questions.cpp
@@ -10,11 +10,41 @@
 //         cout << "Max = " <<max<< endl;
 // }
 
+// #include<iostream>
+// using namespace std;
+
+// int main () {
+//     int x,y;
+//     cin >> x >> y;
+//     if(x==0 && y==0) cout << "Origin";
+//     else if(x==0 && y==0) cout << "Origin";
+//     else if(x!=0 && y==0) cout << "X axis";
+//     else if(x==0 && y!=0) cout << "Y axis";
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// int main () {
+//     int n, f;
+//     cin >> n >> f;
+//     n % f == 0 ? cout << "Yes" : cout << "No";
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// int main () {
+//     int n;
+//     cin >> n;
+//     for (int i = 1; i <= 10; i++) {
+//         cout << n<<" * " << i << " = " << n * i<<endl;
+//     }
+// }
+
 #include<iostream>
 using namespace std;
 
 int main () {
-    int x,y;
-    cin >> x >> y;
-    if(x==0 && y==0) cout << "Origin";
+
 }
