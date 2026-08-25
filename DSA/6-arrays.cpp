be93a890f3f372de236c8132fbdcbf5bc0ee5244
priyambuilds <dpriyam45@gmail.com>
2026-08-26 01:02:52 +0530

    update

--- DSA/6-arrays.cpp
+++ DSA/6-arrays.cpp
@@ -1,3 +1,51 @@
+// #include<iostream>
+// using namespace std;
+
+// int main () {
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i = 0; i <= n; i++) {
+//         cin>>arr[i];
+//     }
+
+//     for(int num : arr) {
+//         cout<<num<<" ";
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// int main () {
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i = 0; i <= n; i++) {
+//         cin>>arr[i];
+//     }
+//     for (int i = n-1; i >= 0; i--) {
+//         cout<<arr[i]<<" ";
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// int main () {
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i = 0; i < n; i++) {
+//         cin>>arr[i];
+//     }
+//     int sum = 0;
+//     for (int i = n-1; i >= 0; i--) {
+//         sum += arr[i];
+//     }
+//     cout<<sum;
+// }
+
 #include<iostream>
 using namespace std;
 
@@ -5,11 +53,119 @@ int main () {
     int n;
     cin>>n;
     int arr[n];
-    for(int i = 0; i <= n; i++) {
+    for(int i = 0; i < n; i++) {
         cin>>arr[i];
     }
-
-    for(int num : arr) {
-        cout<<num<<" ";
+    int max = arr[0];
+    int index = 1;
+    for (int i = 1; i < n; i++)
+    {
+        if(arr[i]<max) {
+            max = arr[i], index = i+1;
+        }
     }
+    cout << max<<" "<<index;
 }
+
+// #include<iostream>
+// using namespace std;
+
+// int main () {
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i = 0; i < n; i++) {
+//         cin>>arr[i];
+//     }
+//     int max = arr[0];
+//     int index = 1;
+//     for (int i = 1; i < n; i++)
+//     {
+//         if(arr[i]>max) {
+//             max = arr[i], index = i+1;
+//         }
+//     }
+//     cout << max<<" "<<index;
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// int main () {
+//     int n, x;
+//     cin>>n>>x;
+//     int arr[n];
+//     for (int i = 0; i < n; i++) {
+//         cin>>arr[i];
+//         if(arr[i] == x) {
+//             cout<<"YES";
+//             return 0;
+//         }
+//     }
+//     cout<<"NO";
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// int main () {
+//     int n, x;
+//     cin>>n>>x;
+//     int arr[n];
+//     int count = 0;
+//     for (int i = 0; i < n; i++) {
+//         cin>>arr[i];
+//         if(arr[i] == x) {
+//             count++;
+//         }
+//     }
+//     cout << count;
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// int main () {
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++) {
+//         cin>>arr[i];
+//     }
+//     for (int i = 1; i < n; i++) {
+//         if (arr[i] < arr[i - 1])
+//         {
+//             cout << "NO";
+//             return 0;
+//         }
+//     }
+//     cout<<"YES";
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// int main () {
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     int c0 = 0, c1 = 0;
+//     for (int i = 0; i < n; i++) {
+//         cin>>arr[i];
+//         if(arr[i] == 0) {
+//             c0++;
+//         } else c1++;
+//     }
+//     for (int i = 1; i <= c0; i++) {
+//         arr[i] = 0;
+//     }
+//     for (int i = c0 ; i < n; i++) {
+//         arr[i] = 1;
+//     }
+
+//     for (int i = 0; i < n; i++) {
+//         cout<<arr[i]<< " ";
+//     }
+// }
