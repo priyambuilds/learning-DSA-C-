a92f6fe1d5a4be5f985e585a67c57b25c81148a0
priyambuilds <dpriyam45@gmail.com>
2026-09-05 02:04:37 +0530

    update

---
+++ DSA/C_Lowercase_Uppercase_Digit_or_Special.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cin>>ch;
+    if (ch >= 'A' && ch <= 'Z') cout << "Uppercase";
+    else if (ch >= 'a' && ch <= 'z') cout << "Lowercase";
+    else if (ch >= '0' && ch <= '9') cout << "Digit";
+    else cout << "Special";
+}
